2ac43896c9d2fe0eaae5deb5c90d18b66521c849
Patrick White <whitep@vcu.edu>
2015-12-29 16:29:30 -0500

    ripped out jenkins stuff

--- docker/jenkins/groovy/purgeBuilds.groovy
+++
@@ -1,7 +0,0 @@
-import hudson.model.*
-
-def jobs = Jenkins.instance.projects.collect { it.name }
-jobs.each {
-  def jobi = Jenkins.instance.getItem( it )
-  jobi.getBuilds().each { it.delete() }
-}
